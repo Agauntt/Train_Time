aaa2d797574a7a1197e7fddf260f705d7ffdc264
Aaron_G <gauntta@gmail.com>
2019-02-17 13:10:23 -0600

    switching machines

--- assets/logic.js
+++ assets/logic.js
@@ -18,49 +18,49 @@ var trains = [
     {
         train: "Trenton Express",   
         destination: "Trenton",
-        frequency: "",
+        frequency: "25",
         nextArrival: "",
         minutesAway: "",
     },
     {
         train: "Oregon Trail",
         destination: "Salem, Oregon",
-        frequency: "",
+        frequency: "200",
         nextArrival: "",
         minutesAway: "",
     },
     {
         train: "Midnight Carriage",
         destination: "Philadelphia",
-        frequency: "",
+        frequency: "15",
         nextArrival: "",
         minutesAway: "",
     },
     {
         train: "Sing Sing Caravan",
         destination: "Atlanta",
-        frequency: "",
+        frequency: "3000",
         nextArrival: "",
         minutesAway: ""
     },
     {
         train: "Boston Bus",
         destination: "Boston",
-        frequency: "",
+        frequency: "60",
         nextArrival: "",
         minutesAway: ""
     },
     {
         train: "California Caravan",
         destination: "San Francisco",
-        frequency: "",
+        frequency: "100",
         nextArrival: "",
         minutesAway: ""
     },
     {
         train: "Analben's Train",
         destination: "Florida",
-        frequency: "",
+        frequency: "90",
         nextArrival: "",
         minutesAway: ""
     },
@@ -69,9 +69,18 @@ var trains = [
 function trainSchedule(){
 //Logic to populate the table
 for (i = 0; i < trains.length; i++) {
-    card.append("<tr><td>" + trains[i].train + "</td><td>" + trains[i].destination);
+    card.append("<tr><td>" + trains[i].train + "</td><td>" + trains[i].destination + "</td><td>" + trains[i].frequency);
     console.log(trains[i].train);
 }
 }
 
+//populates the table with the trains from the array
 trainSchedule();
+
+//Generates the current time
+var date = new Date(); 
+var t = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+console.log(date);
+console.log(t);
+
